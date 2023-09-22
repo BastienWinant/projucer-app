26e1023d914cba93646db3653da0e931c23364b7
Armand Winant <145713109+BastienWinant@users.noreply.github.com>
2023-09-22 23:30:00 +0200

    add pausing ability

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -2,7 +2,6 @@
 
 //==============================================================================
 MainComponent::MainComponent()
-: state(Stopped)
 {
     // Make sure you set the size of the component after
     // you add any child components.
@@ -33,6 +32,11 @@ MainComponent::MainComponent()
     addAndMakeVisible(volSlider);
     addAndMakeVisible(speedSlider);
     addAndMakeVisible(posSlider);
+    addAndMakeVisible(loadButton);
+    
+    playButton.addListener(this);
+    stopButton.addListener(this);
+    loadButton.addListener(this);
     
     // set application to initial state
     changeState(Initial);
@@ -49,22 +53,26 @@ void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRat
 {
     // This function will be called when the audio device is started, or when
     // its settings (i.e. sample rate, block size, etc) are changed.
+    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
 }
 
 void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
 {
-    // Right now we are not producing any data, in which case we need to clear the buffer
-    // (to prevent the output of random noise)
+    if (readerSource.get() == nullptr)
+    {
+        // Clear the buffer so as to prevent random noise
+        bufferToFill.clearActiveBufferRegion();
+        return;
+    }
     
-    // Clear the buffer so as to prevent random noise
-    bufferToFill.clearActiveBufferRegion();
+    transportSource.getNextAudioBlock(bufferToFill);
 }
 
 void MainComponent::releaseResources()
 {
     // This will be called when the audio device stops, or when it is being
     // restarted due to a setting change.
-
+    transportSource.releaseResources();
 }
 
 //==============================================================================
@@ -81,13 +89,14 @@ void MainComponent::resized()
     // This is called when the MainContentComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
-    double rowH = getHeight() / 5;
+    double rowH = getHeight() / 6;
     
     playButton.setBounds(0, 0, getWidth(), rowH);
     stopButton.setBounds(0, rowH, getWidth(), rowH);
     volSlider.setBounds(0, rowH * 2, getWidth(), rowH);
     speedSlider.setBounds(0, rowH * 3, getWidth(), rowH);
     posSlider.setBounds(0, rowH * 4, getWidth(), rowH);
+    loadButton.setBounds(0, rowH * 5, getWidth(), rowH);
 }
 
 void MainComponent::changeState(AppState newState)
@@ -103,18 +112,36 @@ void MainComponent::changeState(AppState newState)
                 playButton.setEnabled(false);
                 transportSource.setPosition(0.0);
                 break;
+                
             case Stopped:
+                playButton.setButtonText("PLAY");
+                stopButton.setButtonText("STOP");
                 stopButton.setEnabled(false);
-                playButton.setEnabled(true);
                 transportSource.setPosition(0.0);
                 break;
+                
             case Starting:
-                playButton.setEnabled(false);
+                // starting the transport source will eventually transition to Playing state
                 transportSource.start();
                 break;
+                
             case Playing:
+                playButton.setButtonText("PAUSE");
+                stopButton.setButtonText("STOP");
                 stopButton.setEnabled(true);
                 break;
+                
+            case Pausing:
+                // pausing the transport source will eventually transition to Paused state
+                transportSource.stop();
+                break;
+                
+            case Paused:
+                // change the buttons' text when reaching a paused state
+                playButton.setButtonText("RESUME");
+                stopButton.setButtonText("RETURN TO ZERO");
+                break;
+                
             case Stopping:
                 transportSource.stop();
                 break;
@@ -130,9 +157,75 @@ void MainComponent::changeListenerCallback (juce::ChangeBroadcaster *source)
         {
             changeState(Playing);
         }
+        else if ((state == Stopping) || (state == Playing))
+        {
+            changeState(Stopped);
+        }
+        else if (state == Pausing)
+        {
+            changeState(Paused);
+        }
+    }
+}
+
+void MainComponent::buttonClicked (juce::Button *button)
+{
+    if (button == &playButton)
+    {
+        if ((state == Stopped) || (state == Paused)) // resume play from the stopped or paused state
+        {
+            changeState(Starting);
+        }
+        else if (state == Playing) // the play button is a pause button in this scenario
+        {
+            changeState(Pausing);
+        }
+    }
+    
+    if (button == &stopButton)
+    {
+        if (state == Paused)
+        {
+            changeState(Stopped);
+        }
         else
         {
             changeState(Stopping);
         }
     }
+    
+    if (button == &loadButton)
+    {
+        loadFile();
+    }
+}
+
+void MainComponent::sliderValueChanged (juce::Slider *slider)
+{}
+
+void MainComponent::loadFile()
+{
+    chooser = std::make_unique<juce::FileChooser> ("Select an MP3 file to play...",
+                                                   juce::File{}, "*.mp3");
+    
+    auto chooserFlags = juce::FileBrowserComponent::openMode |
+                        juce::FileBrowserComponent::canSelectFiles;
+    
+    chooser->launchAsync(chooserFlags, [this] (const juce::FileChooser& fc)
+    {
+        juce::File file = fc.getResult();
+        
+        if (file != juce::File{})
+        {
+            auto* reader = formatManager.createReaderFor(file);
+            
+            if (reader != nullptr)
+            {
+                auto newSource = std::make_unique<juce::AudioFormatReaderSource> (reader, true);
+                transportSource.setSource(newSource.get(), 0, nullptr, reader->sampleRate);
+                playButton.setEnabled(true);
+                readerSource.reset(newSource.release());
+            }
+        }
+    });
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -5,13 +5,18 @@
 enum AppState
 {
     Initial, // Define a state for the intial state of the app
-    Stopping, // Audio is playing but playback has been told to stop, after this it will return to the Stopped state
     Stopped, // Audio playback is stopped and ready to be started
     Starting, // Audio playback hasn't yet started but it has been told to start
-    Playing // Audio is playing
+    Playing, // Audio is playing
+    Pausing,
+    Paused,
+    Stopping, // Audio is playing but playback has been told to stop, after this it will return to the Stopped state
 };
 
-class MainComponent  : public juce::AudioAppComponent, public juce::ChangeListener
+class MainComponent  : public juce::AudioAppComponent,
+                        public juce::ChangeListener,
+                        public juce::Button::Listener,
+                        public juce::Slider::Listener
 {
 public:
     //==============================================================================
@@ -33,11 +38,29 @@ public:
     // ChangeListener class methods
     void changeListenerCallback (juce::ChangeBroadcaster *source) override;
     
+    //==============================================================================
+    // Button::Listener class methods
+    void buttonClicked (juce::Button *button) override;
+    
+    //==============================================================================
+    // Button::Listener class methods
+    void sliderValueChanged (juce::Slider *slider) override;
+    
     void changeState(AppState newState);
+    
+    void loadFile();
 
 private:
     //==============================================================================
     AppState state;
+    
+    // Buttons and sliders
+    juce::TextButton playButton{"PLAY"};
+    juce::TextButton stopButton{"STOP"};
+    juce::Slider volSlider;
+    juce::Slider speedSlider;
+    juce::Slider posSlider;
+    juce::TextButton loadButton{"LOAD"};
     
     // Audio file reading and rendering components
     /** contains list of audio formats and can create suitable objects for reading audio data */
@@ -47,12 +70,7 @@ private:
     /** control the playback of an AudioFormatReaderSource object */
     juce::AudioTransportSource transportSource;
     
-    // Buttons and sliders
-    juce::TextButton playButton{"PLAY"};
-    juce::TextButton stopButton{"STOP"};
-    juce::Slider volSlider;
-    juce::Slider speedSlider;
-    juce::Slider posSlider;
+    std::unique_ptr<juce::FileChooser> chooser;
 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
