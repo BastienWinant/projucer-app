74766797e0c3b9d5979cb85388bdeed90e142696
Armand Winant <145713109+BastienWinant@users.noreply.github.com>
2023-10-05 22:32:39 +0200

    reinitialise files

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -19,27 +19,6 @@ MainComponent::MainComponent()
         // Specify the number of input and output channels that we want to open
         setAudioChannels (2, 2);
     }
-    
-    // initialise the AudioFormatManager object to register a list of standard formats
-    formatManager.registerBasicFormats();
-    
-    // add MainContentComponent object as listener to the AudioTransportSource object so that we can respond to changes in its state
-    transportSource.addChangeListener(this);
-    
-    // add the buttons and sliders to the display
-    addAndMakeVisible(playButton);
-    addAndMakeVisible(stopButton);
-    addAndMakeVisible(volSlider);
-    addAndMakeVisible(speedSlider);
-    addAndMakeVisible(posSlider);
-    addAndMakeVisible(loadButton);
-    
-    playButton.addListener(this);
-    stopButton.addListener(this);
-    loadButton.addListener(this);
-    
-    // set application to initial state
-    changeState(Initial);
 }
 
 MainComponent::~MainComponent()
@@ -53,26 +32,30 @@ void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRat
 {
     // This function will be called when the audio device is started, or when
     // its settings (i.e. sample rate, block size, etc) are changed.
-    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+
+    // You can use this function to initialise any resources you might need,
+    // but be careful - it will be called on the audio thread, not the GUI thread.
+
+    // For more details, see the help for AudioProcessor::prepareToPlay()
 }
 
 void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
 {
-    if (readerSource.get() == nullptr)
-    {
-        // Clear the buffer so as to prevent random noise
-        bufferToFill.clearActiveBufferRegion();
-        return;
-    }
-    
-    transportSource.getNextAudioBlock(bufferToFill);
+    // Your audio-processing code goes here!
+
+    // For more details, see the help for AudioProcessor::getNextAudioBlock()
+
+    // Right now we are not producing any data, in which case we need to clear the buffer
+    // (to prevent the output of random noise)
+    bufferToFill.clearActiveBufferRegion();
 }
 
 void MainComponent::releaseResources()
 {
     // This will be called when the audio device stops, or when it is being
     // restarted due to a setting change.
-    transportSource.releaseResources();
+
+    // For more details, see the help for AudioProcessor::releaseResources()
 }
 
 //==============================================================================
@@ -89,143 +72,4 @@ void MainComponent::resized()
     // This is called when the MainContentComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
-    double rowH = getHeight() / 6;
-    
-    playButton.setBounds(0, 0, getWidth(), rowH);
-    stopButton.setBounds(0, rowH, getWidth(), rowH);
-    volSlider.setBounds(0, rowH * 2, getWidth(), rowH);
-    speedSlider.setBounds(0, rowH * 3, getWidth(), rowH);
-    posSlider.setBounds(0, rowH * 4, getWidth(), rowH);
-    loadButton.setBounds(0, rowH * 5, getWidth(), rowH);
-}
-
-void MainComponent::changeState(AppState newState)
-{
-    if (state != newState)
-    {
-        state = newState;
-        
-        switch(state)
-        {
-            case Initial:
-                stopButton.setEnabled(false);
-                playButton.setEnabled(false);
-                transportSource.setPosition(0.0);
-                break;
-                
-            case Stopped:
-                playButton.setButtonText("PLAY");
-                stopButton.setButtonText("STOP");
-                stopButton.setEnabled(false);
-                transportSource.setPosition(0.0);
-                break;
-                
-            case Starting:
-                // starting the transport source will eventually transition to Playing state
-                transportSource.start();
-                break;
-                
-            case Playing:
-                playButton.setButtonText("PAUSE");
-                stopButton.setButtonText("STOP");
-                stopButton.setEnabled(true);
-                break;
-                
-            case Pausing:
-                // pausing the transport source will eventually transition to Paused state
-                transportSource.stop();
-                break;
-                
-            case Paused:
-                // change the buttons' text when reaching a paused state
-                playButton.setButtonText("RESUME");
-                stopButton.setButtonText("RETURN TO ZERO");
-                break;
-                
-            case Stopping:
-                transportSource.stop();
-                break;
-        }
-    }
-}
-
-void MainComponent::changeListenerCallback (juce::ChangeBroadcaster *source)
-{
-    if (source == &transportSource)
-    {
-        if (transportSource.isPlaying())
-        {
-            changeState(Playing);
-        }
-        else if ((state == Stopping) || (state == Playing))
-        {
-            changeState(Stopped);
-        }
-        else if (state == Pausing)
-        {
-            changeState(Paused);
-        }
-    }
-}
-
-void MainComponent::buttonClicked (juce::Button *button)
-{
-    if (button == &playButton)
-    {
-        if ((state == Stopped) || (state == Paused)) // resume play from the stopped or paused state
-        {
-            changeState(Starting);
-        }
-        else if (state == Playing) // the play button is a pause button in this scenario
-        {
-            changeState(Pausing);
-        }
-    }
-    
-    if (button == &stopButton)
-    {
-        if (state == Paused)
-        {
-            changeState(Stopped);
-        }
-        else
-        {
-            changeState(Stopping);
-        }
-    }
-    
-    if (button == &loadButton)
-    {
-        loadFile();
-    }
-}
-
-void MainComponent::sliderValueChanged (juce::Slider *slider)
-{}
-
-void MainComponent::loadFile()
-{
-    chooser = std::make_unique<juce::FileChooser> ("Select an MP3 file to play...",
-                                                   juce::File{}, "*.mp3");
-    
-    auto chooserFlags = juce::FileBrowserComponent::openMode |
-                        juce::FileBrowserComponent::canSelectFiles;
-    
-    chooser->launchAsync(chooserFlags, [this] (const juce::FileChooser& fc)
-    {
-        juce::File file = fc.getResult();
-        
-        if (file != juce::File{})
-        {
-            auto* reader = formatManager.createReaderFor(file);
-            
-            if (reader != nullptr)
-            {
-                auto newSource = std::make_unique<juce::AudioFormatReaderSource> (reader, true);
-                transportSource.setSource(newSource.get(), 0, nullptr, reader->sampleRate);
-                playButton.setEnabled(true);
-                readerSource.reset(newSource.release());
-            }
-        }
-    });
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -2,21 +2,12 @@
 
 #include <JuceHeader.h>
 
-enum AppState
-{
-    Initial, // Define a state for the intial state of the app
-    Stopped, // Audio playback is stopped and ready to be started
-    Starting, // Audio playback hasn't yet started but it has been told to start
-    Playing, // Audio is playing
-    Pausing,
-    Paused,
-    Stopping, // Audio is playing but playback has been told to stop, after this it will return to the Stopped state
-};
-
-class MainComponent  : public juce::AudioAppComponent,
-                        public juce::ChangeListener,
-                        public juce::Button::Listener,
-                        public juce::Slider::Listener
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainComponent  : public juce::AudioAppComponent
 {
 public:
     //==============================================================================
@@ -24,53 +15,17 @@ public:
     ~MainComponent() override;
 
     //==============================================================================
-    // AudioSource class methods
     void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
     void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override;
     void releaseResources() override;
 
     //==============================================================================
-    // Component class methods
     void paint (juce::Graphics& g) override;
     void resized() override;
-    
-    //==============================================================================
-    // ChangeListener class methods
-    void changeListenerCallback (juce::ChangeBroadcaster *source) override;
-    
-    //==============================================================================
-    // Button::Listener class methods
-    void buttonClicked (juce::Button *button) override;
-    
-    //==============================================================================
-    // Button::Listener class methods
-    void sliderValueChanged (juce::Slider *slider) override;
-    
-    void changeState(AppState newState);
-    
-    void loadFile();
 
 private:
     //==============================================================================
-    AppState state;
-    
-    // Buttons and sliders
-    juce::TextButton playButton{"PLAY"};
-    juce::TextButton stopButton{"STOP"};
-    juce::Slider volSlider;
-    juce::Slider speedSlider;
-    juce::Slider posSlider;
-    juce::TextButton loadButton{"LOAD"};
-    
-    // Audio file reading and rendering components
-    /** contains list of audio formats and can create suitable objects for reading audio data */
-    juce::AudioFormatManager formatManager;
-    /** reads audio data from an AudioFormatReader object and render the audio */
-    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
-    /** control the playback of an AudioFormatReaderSource object */
-    juce::AudioTransportSource transportSource;
-    
-    std::unique_ptr<juce::FileChooser> chooser;
+    // Your private member variables go here...
 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
